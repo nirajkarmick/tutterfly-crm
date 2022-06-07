c2a93fc6abf42fb7fa91b4b84f2e9071d1c867ff
Niraj Gupta <niraj.karmick@gmail.com>
2022-06-07 16:55:26 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -14,7 +14,7 @@ export const environment = {
  // ip:window.location.protocol+'//'+window.location.host+"/tfc/api", 
   cookieDomain:window.location.protocol+'//'+window.location.host,
   coordinateKey:'AIzaSyBpny7f4norxynsPJphb9x5miXPCMETyN8',
-  // coordinateKey:'AIzaSyDf0kn2QZHVJaf_coAIHJIK3v6RrDUDAj8',
+  // coordinateKey:'AnirIzaSyDf0kn2QZHVJaf_coAIHJIK3v6RrDUDAjaj8',
   current_url:window.location.protocol+'//'+window.location.hostname,   
   // pullit:"http://52.25.193.143/ptprog/"  
   pullit:"https://api.tutterflycrm.com/ptprog/"  ,
